d77c2579578c3083ab51ca817ed990d0b89cb033
uraura <t@urau.la>
2017-10-30 01:17:10 +0900

    💩

--- bin/auto-commit.sh
+++ bin/auto-commit.sh
@@ -14,6 +14,7 @@ notify() {
 }
 
 trap 'notify' ERR
+git add --all
 git commit --all --message "$emoji"
 git push origin master
 osascript -e "display notification \"dotfiles saved!\" with title \"$emoji\""
---
+++ bin/brew-maint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+notify() {
+  osascript -e 'display notification "failed to save dotfiles." with title "🆘 "'
+  exit 1
+}
+
+trap 'notify' ERR
+
+$HOME/bin/brew update
+$HOME/bin/brew upgrade
